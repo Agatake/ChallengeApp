d1bca6a8ac233d33934948f2f779f216f7e6b12e
Agata Brożek <agata.brozek@onet.pl>
2024-03-06 18:39:36 +0100

    Praca domowa dzień 4 v4

---
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -0,0 +1,15 @@
+﻿var name = "Ewa";
+var sex = "female";
+var age = 30;
+if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex == "female" && age < 30)
+{
+    Console.WriteLine("kobieta poniżej 30 lat");
+}
+else if (sex == "male" && age < 18)
+{
+    Console.WriteLine("niepełnoletni mężczyzna");
+}
